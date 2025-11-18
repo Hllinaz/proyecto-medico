f3fa8e8f9566281fe451128a1d12b04f32a0e352
Hllinaz <82002515+Hllinaz@users.noreply.github.com>
2025-11-17 19:28:07 -0500

    Orden

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { 
+    path: 'citas', 
+    loadChildren: () => import('./modules/cita/cita.module').then(m => m.CitaModule)
+  },
+  { 
+    path: 'medicos', 
+    loadChildren: () => import('./modules/medico/medico.module').then(m => m.MedicoModule)
+  },
+  { 
+    path: 'pacientes', 
+    loadChildren: () => import('./modules/paciente/paciente.module').then(m => m.PacienteModule)
+  },
+  { path: '', redirectTo: '/citas', pathMatch: 'full' }
+];
