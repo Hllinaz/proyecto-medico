3ac5464634f3efe9d3d34e5bd15d397c4c0c7d6d
Hllinaz <82002515+Hllinaz@users.noreply.github.com>
2025-11-17 19:32:17 -0500

    orden 2

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -4,8 +4,8 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  templateUrl: '../app.html',
+  styleUrl: '../app.css'
 })
 export class AppComponent {
   protected readonly title = signal('proyecto');
